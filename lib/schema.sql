03cc794b9a8987bd9fe2e9ce037eb26a592fc10a
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-16 21:25:18 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -10,6 +10,11 @@ CREATE TABLE subgenres (
   name TEXT
 );
 
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
   title TEXT,
@@ -17,11 +22,6 @@ CREATE TABLE books (
   series_id INTEGER
 );
 
-
-CREATE TABLE authors (
-  id INTEGER PRIMARY KEY,
-  name TEXT
-);
 
 CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
