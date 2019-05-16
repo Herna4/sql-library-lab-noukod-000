97509822944164341d580ce43ca5dc582ec120e7
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-16 21:49:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,10 +14,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
- "SELECT characters.species, 
-  COUNT(*) 
-  FROM characters 
-  GROUP BY species 
+ "SELECT characters.species,
+  COUNT(*)
+  FROM characters
+  GROUP BY species
   ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
